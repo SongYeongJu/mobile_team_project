df02785c7040f69d24f99cf0e551d9f0ce5e6a63
SongYeongJu <thddudwn1012@naver.com>
2017-12-14 06:02:19 +0900

    select item 액티비티에 아이템에 알맞게 텍스트뷰를 채우도록함
    아이템에 맞게 텍스트 뷰를 채우는 함수를 만듦
    지도 구현후 이 함수만 쓰면 됨

--- MyApplication/app/src/main/java/com/example/myapplication/DataStructure/Item.java
+++ MyApplication/app/src/main/java/com/example/myapplication/DataStructure/Item.java
@@ -23,21 +23,26 @@ public class Item implements Serializable {
     private String request;
 
 
-    public Item() {}
+    public static Item returnSempleItem() {
+        return new Item("sample", 1, "상", 0, 0, new Date(), 0, 0, new Date(), null, false);
+    }
 
-    public Item(String name,int weight,String size,int dx,int dy,Date dt,int sx,int sy,Date st,String request,boolean order){
+    public Item() {
+    }
+
+    public Item(String name, int weight, String size, int dx, int dy, Date dt, int sx, int sy, Date st, String request, boolean order) {
         setName(name);
         setWeight(weight);
         setSize(size);
-        setDes(dx,dy);
+        setDes(dx, dy);
         setDt(dt);
-        setSta(sx,sy);
+        setSta(sx, sy);
         setSt(st);
         setRequest(request);
         setOrder(order);
     }
 
-    public Item(String name,int weight,String size,Location ld,Date dt,Location ls,Date st,String request,boolean order){
+    public Item(String name, int weight, String size, Location ld, Date dt, Location ls, Date st, String request, boolean order) {
         setName(name);
         setWeight(weight);
         setSize(size);
@@ -49,55 +54,79 @@ public class Item implements Serializable {
         setOrder(order);
     }
 
-    public void setSta(int x,int y) { this.sta=new Location(x,y); }
-    public void setDes(int x,int y) { this.des=new Location(x,y); }
-    public void setSize(String size) { this.size= size; }
+    public void setSta(int x, int y) { this.sta = new Location(x, y); }
+
+    public void setDes(int x, int y) { this.des = new Location(x, y); }
+
+    public void setSize(String size) { this.size = size; }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setWeight(int weight) {
         this.weight = weight;
     }
+
     public void setDes(Location des) {
         this.des = des;
     }
+
     public void setSt(Date st) {
         this.st = st;
     }
+
     public void setSta(Location sta) {
         this.sta = sta;
     }
+
     public void setDt(Date dt) {
         this.dt = dt;
     }
+
     public void setOrder(boolean order) {
         this.order = order;
     }
-    public void setRequest(String request) {this.request = request;}
 
-    public String getRequest() {return request;}
+    public void setRequest(String request) { this.request = request; }
+
+    public String getRequest() { return request; }
+
     public String getName() {
         return name;
     }
+
     public int getWeight() {
         return weight;
     }
+
     public Location getDes() {
         return des;
     }
+
     public Date getSt() {
         return st;
     }
+
     public Location getSta() {
         return sta;
     }
+
     public Date getDt() {
         return dt;
     }
+
     public String getSize() { return size; }
 
     public boolean isOrder() {
         return order;
+    }
+
+    public String returnInfo() {
+        String s;
+        String en = "\n";
+        s = "크기 : " + getSize() + en + "무게 : " + getWeight() + en + "배달 시작 시간: " + getSt().toString() + en + "배달 종료 시간 :" + getDt().toString() + en + "요청 사항 : " + getRequest();
+        return s;
     }
 
 }
